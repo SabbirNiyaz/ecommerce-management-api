d1135fd4a1138c57aa0409a28ab51bd195330700
SabbirNiyaz <sabbirlm10niyaz@gmail.com>
2026-02-21 21:50:09 +0600

    Add Validation Pipes (DTO)

--- src/seller/dto/create-product.dto.ts
+++ src/seller/dto/create-product.dto.ts
@@ -1,25 +1,29 @@
+import { Type } from 'class-transformer';
 import { IsString, IsNumber, IsNotEmpty, Min } from 'class-validator';
 
 export class CreateProductDto {
-    @IsString()
-    @IsNotEmpty()
+
+    @IsString({ message: 'Product name must be a string' })
+    @IsNotEmpty({ message: 'Product name is required' })
     name: string;
 
-    @IsString()
-    @IsNotEmpty()
+    @IsString({ message: 'Category must be a string' })
+    @IsNotEmpty({ message: 'Category is required' })
     category: string;
 
-    @IsNumber()
-    @IsNotEmpty()
-    @Min(1)
+    @Type(() => Number)
+    @IsNumber({}, { message: 'Price must be a number' })
+    @IsNotEmpty({ message: 'Price is required' })
+    @Min(1, { message: 'Price must be at least 1' })
     price: number;
 
-    @IsNumber()
-    @IsNotEmpty()
-    @Min(0)
+    @Type(() => Number)
+    @IsNumber({}, { message: 'Stock must be a number' })
+    @IsNotEmpty({ message: 'Stock is required' })
+    @Min(0, { message: 'Stock cannot be negative' })
     stock: number;
 
-    @IsString()
-    @IsNotEmpty()
+    @IsString({ message: 'Description must be a string' })
+    @IsNotEmpty({ message: 'Description is required' })
     description: string;
 }
